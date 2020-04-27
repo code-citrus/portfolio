b3cb6dda45647f6a085bca7dcff737b870f329c9
gautham <gautham.g@live.com>
2020-04-27 12:25:13 +0200

    Responsive sidebar

--- src/App.js
+++ src/App.js
@@ -5,34 +5,37 @@ class App extends Component {
   render() {
     return(
       <div>
-        <div className="sidebar">
-          <div className="sidebar-main">
-            <div>
-              <div className="namecard">
-                <div className="salutation">Hello! My name is,</div>
-                <div className="name">Gautham</div>
+        <div className="sb-container">
+          <div className="sidebar">
+            <div className="sidebar-main">
+              <div className="navbox">
+                <div className="namecard">
+                  <div className="salutation">Hello! My name is,</div>
+                  <div className="name">Gautham</div>
+                </div>
+                <nav>
+                  <a href="#">About</a>
+                  <a href="#">Skills</a>
+                  <a href="#">Experience</a>
+                  <a href="#">Portfolio</a>
+                </nav>
               </div>
-              <nav>
-                <a href="#">About</a>
-                <a href="#">Skills</a>
-                <a href="#">Experience</a>
-                <a href="#">Portfolio</a>
-              </nav>
+              <div className="intro">
+                <p>
+                  I have worked in software for over 5 years and with software since I was a kid.
+                  I am inspired by finding simple and elegant approaches to complex problems.
+                  As a full-stack engineer with a solid analytical background,
+                  I am available to help you not only solve but also understand your technical challenges.
+                </p>
+              </div>
             </div>
-            <div className="intro">
-              I have worked in software for over 5 years and with software since I was a kid.
-              I am inspired by finding simple and elegant approaches to complex problems.
-              As a full-stack engineer with a solid analytical background,
-              I am available to help you not only solve but also understand your technical challenges.
-              </div>
-          </div>
-          <div class="flex-break"></div>
-          <div class="skillbar">
-            <ul>
-              <li>Javascript</li>
-              <li>AWS</li>
-              <li>HTML/CSS</li>
-            </ul>
+            <div className="skillbar">
+              <ul>
+                <li>Javascript</li>
+                <li>AWS</li>
+                <li>HTML/CSS</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="hero-border">
