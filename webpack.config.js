a35d969fd3469b2fa7f88e1725374f735e4680a3
gautham <gautham.g@live.com>
2020-05-08 12:49:24 +0200

    Get dev server to work with new asset pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,6 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: [{
           loader: 'file-loader',
-          options: {
-            outputPath: 'assets/',
-          }
         }],
       }
     ]
@@ -33,17 +30,26 @@ module.exports = {
 
   /*
   * The bundle will be written to the dist/ directory.
+  * The public-path setting is used for url-rewriting.
+  * For example, in our case, when CSS-Loader encounters
+  * a css file with a url(foo.jpg) directive, that directive
+  * will cause foo.jpg to be loaded with file-loader, and
+  * the url(..) to be replaced with the URL returned by
+  * file-loader. The file-loader, in turn, will use the
+  * publicPath setting to prefix the URLs.
   */
   output: {
     path: path.resolve(__dirname, 'dist/'),
     filename: 'bundle.js',
+    publicPath: '/assets/',
   },
 
   /*
    * contentBase:
    * The file sys. location of non-webpack assets.
    * Essentially the docroot. The index.html is
-   * served from here.
+   * served from here. By default, this directory will
+   * be served from server root.
    * 
    * publicPath:
    * By default, WDS will mount the bundles @ server root,
@@ -52,7 +58,7 @@ module.exports = {
    */
   devServer: {
     port: 3000,
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: [ /*index*/ path.join(__dirname, 'public/'), /*assets*/ path.join(__dirname, 'dist')],
     publicPath: 'https://localhost:3000/js/',
     hotOnly: true,
   },
